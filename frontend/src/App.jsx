c07e08abbf4863cff6f316660e4c7bac8d276a77
phe0nix <starlordindia1230@gmail.com>
2026-07-12 22:25:25 +0530

    first frontend deploy

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+
+
+const App = () => {
+  return (
+    <div className='w-full h-screen bg-[#131313] text-white text-4xl italic flex justify-center items-center'>HiveMind</div>
+  )
+}
+
+export default App
