b20b20f231d728a3b0e3b3afbc3e0efbb51f6e25
phe0nix <starlordindia1230@gmail.com>
2026-07-15 22:40:27 +0530

    auth apis in frontend

--- frontend/src/features/auth/services/auth.apis.js
+++ frontend/src/features/auth/services/auth.apis.js
@@ -4,14 +4,54 @@ const AUTH_URL = `${import.meta.env.VITE_BACKEND_URI}/hivemind/auth`;
 
 export const googleLoginApi = async ({ token }) => {
     try {
-        const res = await axios.post(`${AUTH_URL}/google`, { token}, {
+        const res = await axios.post(`${AUTH_URL}/google`, { token }, {
             withCredentials: true
         })
         console.log(res.data.message);
         return res.data
     } catch (error) {
-    console.error("Google login failed:", error.response?.data || error.message);
-  }
+        console.error("Google login failed:", error.response?.data || error.message);
+    }
+}
+
+
+
+export const signUpApi = async ({ name, email, password }) => {
+    try {
+        const res = await axios.post(`${AUTH_URL}/sign-up`, { name, email, password }, {
+            withCredentials: true
+        })
+        console.log(res.data.message)
+        return res.data
+    } catch (error) {
+        console.error("SignUp failed:", error.response?.data || error.message);
+    }
+}
+
+
+export const loginApi = async ({ email, password }) => {
+    try {
+        const res = await axios.post(`${AUTH_URL}/login`, { email, password }, {
+            withCredentials: true
+        })
+        console.log(res.data.message)
+        return res.data
+    } catch (error) {
+        console.error("Login failed:", error.response?.data || error.message);
+    }
+}
+
+
+export const emailVerifyApi = async ({ otp }) => {
+    try {
+        const res = await axios.post(`${AUTH_URL}/email-verify`, { otp }, {
+            withCredentials: true
+        })
+        console.log(res.data.message)
+        
+    } catch (error) {
+        console.error("email verification failed:", error.response?.data || error.message);
+    }
 }
 
 
